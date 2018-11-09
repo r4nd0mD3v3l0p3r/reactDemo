fabb5c76bafe01353068218ff62ee29cf9f24c89
Lorenzo Chiovini <lorenzo.chiovini@philmark.it>
2018-11-09 12:12:24 +0100

    cleanup

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 export default class App extends Component {
   displayName = App.name
@@ -11,9 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
+        // <Route exact path='/' component={Test} />
       </Layout>
     );
   }
