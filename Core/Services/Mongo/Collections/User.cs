b488b475f450dffd9a2d0288a3336cda5176ab51
Lorenzo Chiovini <lorenzo.chiovini@philmark.it>
2018-11-09 11:44:33 +0100

    create and initialize Users collection

---
+++ Core/Services/Mongo/Collections/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reactDemo.Core.Services.Mongo.Collections
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Core/Services/Mongo/DBInitializer.cs
+++ Core/Services/Mongo/DBInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using Mongo2Go;
 using MongoDB.Driver;
+using reactDemo.Core.Services.Mongo.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +11,35 @@ namespace reactDemo.Core.Services.Mongo
     public class DBInitializer : IDisposable, IDBInitializer
     {
         public const string DBName = "reactDemo";
+        public const string UsersCollection = "users";
         readonly MongoDbRunner _runner;
+        readonly MongoClient _client;
 
         public DBInitializer()
         {
             _runner = MongoDbRunner.Start();
 
-            var client = new MongoClient(_runner.ConnectionString);
-            var database = client.GetDatabase(DBName);
+            _client = new MongoClient(_runner.ConnectionString);
+
+            CreateUsersCollection();
         }
 
         public void Dispose()
         {
             if (_runner != null && !_runner.Disposed)
                 _runner.Dispose();
+        }
+
+        public IMongoDatabase GetDatabase() => _client.GetDatabase(DBName);
+
+        void CreateUsersCollection()
+        {
+            var users = new List<User>
+            {
+                new User{ Name = "admin", Password = "admin" }
+            };
+
+            GetDatabase().GetCollection<User>(UsersCollection).InsertMany(users);
         }
     }
 }
