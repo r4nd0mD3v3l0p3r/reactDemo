1ffb9a91cea0caf20e472a43021902deaa6880d0
Lorenzo Chiovini <lorenzo.chiovini@philmark.it>
2019-01-08 10:38:33 +0100

    use dark theme

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,13 +4,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+});
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
+    <BrowserRouter basename={baseUrl}>
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
   </BrowserRouter>,
   rootElement);
 
